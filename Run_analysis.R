f6ae6ae47058eab3bc54b6abbccb4065b2d5013c
Yage0820 <ruyans@gmail.com>
2016-07-25 00:14:40 +0200

    Update Run_analysis.R

--- Run_analysis.R
+++ Run_analysis.R
@@ -1,6 +1,4 @@
 ############Merges the training and the test sets to create one data set#########
-#creat folder
-dir.create("./dataset")
 #download file
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl, destfile = "./dataset/projectData_getCleanData.zip")
